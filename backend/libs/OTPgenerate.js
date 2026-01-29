82feebaa6eb55866e48b30bf7a42699245b10e60
Harsh <userharsh911@gmail.com>
2026-01-29 16:33:38 +0530

    bugs fixed v4

--- backend/libs/OTPgenerate.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export const generateOTP = () => {
-  return Math.floor(100000 + Math.random() * 900000);
-};
-
-export const hashOTP = async (otp) => {
-  return await bcrypt.hash(otp.toString(), 10);
-};
