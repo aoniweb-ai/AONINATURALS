deb10d018045e5fa7a45ec688c6c346a32c31504
Harsh <userharsh911@gmail.com>
2026-03-07 10:56:27 +0530

    meta pixel code implemented

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,27 @@
+if (import.meta.env.VITE_META_ID) {
+  !(function (f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+  window.fbq('init', import.meta.env.VITE_META_ID);
+  window.fbq('track', 'PageView');
+
+  const noscript = document.createElement('noscript');
+  noscript.innerHTML = `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${import.meta.env.VITE_META_ID}&ev=PageView&noscript=1" />`;
+  document.body.appendChild(noscript);
+}
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
